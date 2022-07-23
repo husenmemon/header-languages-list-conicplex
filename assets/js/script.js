3df29fb2d8c8fd02be220b5f3bca35d616318a6b
Husen Memon <memonhusena@gmail.com>
2022-07-23 17:29:41 +0530

    V1.0

---
+++ assets/js/script.js
@@ -0,0 +1,71 @@
+jQuery(document).ready(function ($) {
+  $(document).scroll(function () {
+    // Get document scroll lenght
+    var document_scroll_lenght = $(document).scrollTop();
+
+    // Get document width
+    var document_width = $(document).width();
+
+    // If document width more than 600 then execute the below code
+    if (document_width > 600) {
+      // If document scroll lenght more than 100 then execute the below code
+      if (document_scroll_lenght >= 100) {
+        $header_style =
+          "box-shadow: 0px 0px 12px 0 rgb(0 0 0 / 25%) !important";
+        $("header").attr("style", $header_style);
+        $(".hlcp-main").css({
+          "flex-direction": "column",
+          "align-items": "unset",
+          padding: "12px 5.75%",
+          "box-shadow": "unset",
+          "border-top": "1px solid #cbcbcb",
+        });
+        $(".hlcp-heading").hide();
+        $(".hlcp-heading-on-scroll").show();
+        $(".hlcp-lang-item").css({
+          "flex-direction": "row",
+          margin: "0",
+          "margin-right": "12px",
+          padding: "5px 8px",
+          border: "solid 1px #cbcbcb",
+        });
+        $(".hlcp-country-flag").css({ margin: "0" });
+        $(".hlcp-country-flag img").css({
+          "min-width": "20px",
+          height: "20px",
+          border: "unset",
+          "margin-right": "5px",
+        });
+      } else {
+        $header_style = "box-shadow: unset !important";
+        $("header").attr("style", $header_style);
+        $(".hlcp-main").css({
+          "flex-direction": "row",
+          "align-items": "center",
+          padding: "10px 5.75%",
+          "box-shadow": "rgba(0, 0, 0, 0.25) 0px 0px 7px 0px inset",
+          "max-width": "59em",
+          "border-top": "unset",
+        });
+        $(".hlcp-heading").show();
+        $(".hlcp-heading-on-scroll").hide();
+        $(".hlcp-lang-item").css({
+          "flex-direction": "column",
+          margin: "10px 21px",
+          padding: "unset",
+          border: "unset",
+        });
+        $(".hlcp-country-flag").css({ margin: "7px 0" });
+        $(".hlcp-country-flag img").css({
+          "min-width": "60px",
+          height: "60px",
+          border: "5px solid lightgray",
+          "margin-right": "unset",
+        });
+      }
+    }
+  });
+
+  // Append html output in header
+  $("header").append(hlcp_html_output);
+});
---
+++ header-languages-list-conicplex.php
@@ -0,0 +1,189 @@
+<?php
+/*
+    Plugin Name:  Header Languages List for FluentU Blog by ConicPlex
+    Plugin URI:   https://conicplex.com 
+    Description:  Add and Configure Language Links with their respective flags and show in the header section of the Blog, as per the UI/UX shared.
+    Version:      1.0
+    Author:       Husen Memon (ConicPlex)
+    Author URI:   https://conicplex.com
+    License:      GPL2
+    License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+    Text Domain:  hlcp
+*/
+
+//Exit if Accessed Directly
+if ( ! defined( 'ABSPATH' ) ) {
+        die();
+}
+
+// Include Style & Scripts
+add_action( 'wp_enqueue_scripts', 'hlcp_load_admin_styles' );
+
+function hlcp_load_admin_styles() {
+    wp_enqueue_style('hlcp_style', plugins_url('assets/css/style.css?v='.time(),__FILE__ ));
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('hlcp_script', plugins_url('assets/js/script.js?v='.time(),__FILE__ ),'',false);
+}
+// End Include Style & Scripts
+
+
+// Create Custom Post Type
+function hlcp_custom_post_type() {
+    // Set UI labels for Languages Configurator
+        $labels = array(
+            'name'                => _x( 'Languages', 'Post Type General Name'),
+            'singular_name'       => _x( 'Language', 'Post Type Singular Name'),
+            'menu_name'           => __( 'Languages'),
+            'parent_item_colon'   => __( 'Parent Languages'),
+            'all_items'           => __( 'All Languages'),
+            // 'view_item'           => __( 'View Languages'),
+            'add_new_item'        => __( 'Add New Language'),
+            'add_new'             => __( 'Add New'),
+            'edit_item'           => __( 'Edit Language'),
+            'update_item'         => __( 'Update Language'),
+            'search_items'        => __( 'Search Language'),
+            'not_found'           => __( 'Not Found'),
+            'not_found_in_trash'  => __( 'Not found in Trash'),
+        );
+    // Set other options for Languages Configurator
+        $args = array(
+            'label'               => __( 'Languages'),
+            'description'         => __( 'Languages'),
+            'labels'              => $labels,  
+            'supports'            => array( 'title', 'thumbnail'),     
+            'taxonomies'          => array( 'genres' ),     
+            'hierarchical'        => false,
+            'menu_icon'           => 'dashicons-admin-links',
+            'public'              => true,
+            'capability_type' => 'post',
+					 'capabilities' => array( // allow access to user roles with Administrators only
+						'publish_posts' => 'manage_options',
+						'edit_posts' => 'manage_options',
+						'edit_others_posts' => 'manage_options',
+						'delete_posts' => 'manage_options',
+						'delete_others_posts' => 'manage_options',
+						'read_private_posts' => 'manage_options',
+						'edit_post' => 'manage_options',
+						'delete_post' => 'manage_options',
+						'read_post' => 'manage_options',
+					),
+            'show_ui'             => true,
+            'show_in_menu'        => true,
+            'show_in_nav_menus'   => true,
+            'show_in_admin_bar'   => true,
+            'menu_position'       => 5,
+            'can_export'          => true,
+            'has_archive'         => true,
+            'exclude_from_search' => true,
+            'publicly_queryable'  => false,
+            'capability_type'     => 'post',
+            'show_in_rest' => true, 
+        );
+        // Registering the Languages Configurator
+        register_post_type( 'languages', $args );
+    }
+    add_action( 'init', 'hlcp_custom_post_type', 0 );
+
+
+
+
+    // Create and add Link Field for Languages configurator
+    function hlcp_add_language_url_metabox() {
+        add_meta_box(
+            'language_url_meta_box', // $id
+            'Language Url', // $title
+            'show_the_language_url_meta_box', // $callback
+            'languages', // $screen
+            'normal', // $context
+            'high' // $priority
+        );
+    }
+
+    add_action( 'add_meta_boxes', 'hlcp_add_language_url_metabox' );
+        
+    function show_the_language_url_meta_box() {
+        global $post;  
+        $hlcp_language_url = get_post_meta( $post->ID, 'hlcp_language_url', true ); 
+    ?>
+
+        <input type="hidden" name="language_url_meta_box_nonce" value="<?php echo wp_create_nonce( basename(__FILE__) ); ?>">
+    
+        <!-- All fields will go here -->
+        <p>
+            <input type="text" name="hlcp_language_url" id="hlcp_language_url" style="width: 100%" placeholder="Enter Language Url" value="<?php if (isset($hlcp_language_url)) { echo $hlcp_language_url; } else { echo 'dddd';} ?>">
+        </p>
+    
+    <?php
+    }
+    // End Create custom field for language link
+        
+
+    // Save custom field for language link
+    function hlcp_save_language_url_meta( $post_id ) {
+        
+        // verify nonce
+        if ( !wp_verify_nonce( $_POST['language_url_meta_box_nonce'], basename(__FILE__) ) ) {
+            return $post_id;
+        }
+        // check autosave
+        if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+            return $post_id;
+        }
+        // check permissions
+        if ( 'page' === $_POST['post_type'] ) {
+            if ( !current_user_can( 'edit_page', $post_id ) ) {
+                return $post_id;
+            } elseif ( !current_user_can( 'edit_post', $post_id ) ) {
+                return $post_id;
+            }
+        }
+    
+        $old = get_post_meta( $post_id, 'hlcp_language_url', true );
+        $new = $_POST['hlcp_language_url'];
+
+        if ( $new && $new !== $old ) {
+            update_post_meta( $post_id, 'hlcp_language_url', $new );
+        } elseif ( '' === $new && $old ) {
+            delete_post_meta( $post_id, 'hlcp_language_url', $old );
+        }
+    }
+    add_action( 'save_post', 'hlcp_save_language_url_meta' );
+    
+    // End save custom field for language link    
+
+    // Append data in header
+    function hlcp_js_appned()
+    {   
+        // Get Custom Languages List
+        $get_hlcp_lang = get_posts([
+            'post_type' => 'languages',
+            'post_status' => 'publish',
+            'numberposts' => -1,
+            'order'    => 'ASC'
+        ]);
+
+    ?>
+        <script>
+    
+            var hlcp_html_output = '<div class="hlcp-main"><div class="hlcp-headings"><div class="hlcp-heading"> <span class="hlcp-main-heading">Ready to learn?</span> <span class="hlcp-sub-heading">Pick a language to get started!</span> </div> <div class="hlcp-heading-on-scroll"><span>Pick a language to speak...</span></div></div><div class="hlcp-lang">';
+    <?php
+            
+            foreach($get_hlcp_lang as $hlcp_lang)
+            {
+                
+                $hlcp_language_url = get_post_meta( $hlcp_lang->ID, 'hlcp_language_url',true);
+            ?>
+                hlcp_html_output+= '<a class="hlcp-lang-link" href="<?php echo $hlcp_language_url; ?>"><div class="hlcp-lang-item"><div class="hlcp-country-flag"><img src="<?php echo get_the_post_thumbnail_url($hlcp_lang->ID, "thumbnail"); ?>"></div><div class="hlcp-lang-name"><?php echo $hlcp_lang->post_title; ?></div></div></a>';
+
+            <?php           
+            }
+            ?>
+    
+            hlcp_html_output +='</div></div>';
+
+        </script>
+
+    <?php
+    }
+    add_action( 'wp_head', 'hlcp_js_appned');
+    // End Append data in header
